236371f5a83b85c10a676597740bba4b31db1745
I326319 <ranganath.sn@sap.com>
2021-08-20 00:52:20 +0530

    Home sceen color change

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -9,20 +9,20 @@ function HomeScreen({ navigation }) {
   const [value,setValue] = useState('50');
   return (
     <View style={{ flex: 1 }}>
-      <Card containerStyle={{borderRadius: 10, backgroundColor: 'dodgerblue'}}>
+      <Card containerStyle={{borderRadius: 10, backgroundColor: 'lightgray', borderColor: 'dodgerblue'}}>
         <Card.Title h4>Petrol: 105.25</Card.Title>
         <Card.Divider />
         <Text style={{fontSize: 20, fontWeight:'bold'}}>Total: 3000 lts</Text>
       </Card>
-      <Card containerStyle={{borderRadius: 10, backgroundColor: 'chocolate'}}>
+      <Card containerStyle={{borderRadius: 10, backgroundColor: 'lavender', borderColor: 'chocolate'}}>
         <Card.Title h4>Diesel: 103.23</Card.Title>
         <Card.Divider />
         <Text style={{fontSize: 20, fontWeight:'bold'}}>Total: 1500 lts</Text>
       </Card>
-      <Card containerStyle={{borderRadius: 10, backgroundColor: 'gray'}}>
+      <Card containerStyle={{borderRadius: 10, backgroundColor: 'lightsteelblue', borderColor: 'gray'}}>
         <Card.Title h4>Oil: 20</Card.Title>
         <Card.Divider />
-        <Text style={{fontSize: 20, fontWeight:'bold'}}>Total: 150 lts</Text>
+        <Text style={{fontSize: 20, fontWeight:'bold'}}>Count: 150 </Text>
       </Card>
     </View>
   );
